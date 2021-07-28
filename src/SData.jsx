f053429196576f6e06fd9e70ddcc2b30e07b9dc5
udit2548 <52240868+udit2548@users.noreply.github.com>
2021-07-28 21:45:20 +0530

    App Files Added

---
+++ src/SData.jsx
@@ -0,0 +1,47 @@
+const data=[
+
+{
+    id:1,
+    imgscr:"https://wallpapercave.com/w/wp4056410.jpg" ,
+    title:"A Netflix Orignal Series",
+    sname:"DARK", 
+    link:"https://www.netflix.com/in/title/80100172"
+},
+{
+    id:2,
+    imgscr:"https://wallpapercave.com/w/wp7418473.jpg" ,
+    title:"A Netflix Orignal Series",
+    sname:"Extra-Curricular", 
+    link:"https://www.netflix.com/in/title/80990668"
+},
+{
+    id:3,
+    imgscr:"https://wallpapercave.com/w/wp1839578.jpg" ,
+    title:"A Netflix Orignal Series",
+    sname:"Stranger Things", 
+    link:"https://www.netflix.com/in/title/80057281"
+
+},
+{
+    id:4,
+    imgscr:"https://wallpapercave.com/w/3wuCSpD.jpg" ,
+    title:"Amazon Prime Orignal Series",
+    sname:" The Vampire Diaries", 
+    link:"https://www.primevideo.com/detail/The-Vampire-Diaries/0HG2QOPONEONZPP2ZXNMA6M8Q7"
+},
+{
+    id:5,
+    imgscr:"https://wallpapercave.com/w/wp7285322.jpg" ,
+    title:"Amazon Prime Orignal Series",
+    sname:"Mirzapur ", 
+    link:"https://www.primevideo.com/detail/Mirzapur/0PDOKMV9CRLOMO5EUKNCUJLG4Q"
+},
+{
+    id:6,
+    imgscr:"https://wallpapercave.com/w/wp5152993.jpg" ,
+    title:"A Netflix Original Series",
+    sname:"Money Heist", 
+    link:"https://www.netflix.com/in/title/80192098"
+}
+]
+export default data;
